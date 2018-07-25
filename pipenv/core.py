5f2cd2908928aad5ec4f42735ffad8c34cc42430
Tzu-ping Chung <uranusjr@gmail.com>
2018-07-25 15:55:26 +0800

    Clean up do_sync

--- pipenv/core.py
+++ pipenv/core.py
@@ -2486,7 +2486,6 @@ def do_sync(
     bare=False,
     dont_upgrade=False,
     user=False,
-    verbose=False,
     clear=False,
     unused=False,
     sequential=False,
@@ -2517,7 +2516,7 @@ def do_sync(
     requirements_dir = TemporaryDirectory(suffix="-requirements", prefix="pipenv-")
     do_init(
         dev=dev,
-        verbose=verbose,
+        verbose=(environments.PIPENV_VERBOSITY > 0),
         concurrent=(not sequential),
         requirements_dir=requirements_dir,
         ignore_pipfile=True,  # Don't check if Pipfile and lock match.
